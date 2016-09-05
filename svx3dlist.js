5a6d98dcdd9ccb237e61a134109457c21d7013dd
Julian Todd <julian@goatchurch.org.uk>
2016-09-05 19:00:03 +0200

    move caves js files into caves directory

--- svx3dlist.js
+++ svx3dlist.js
@@ -1,5 +1,7 @@
 LoadSvx3dList([
     {"svxname":"rectangular test", "svxjs":"test.js"},
-    {"svxname":"big china cave", "svxjs":"erin2.js"},
-    {"svxname":"maxpleasure2", "svxjs":"maxpleasure2.js"}
+    {"svxname":"big china cave", "svxjs":"caves/erin2.js"},
+    {"svxname":"maxpleasure2", "svxjs":"caves/maxpleasure2.js"},
+    {"svxname":"Leck Fell Caves", "svxjs":"caves/leckfell.js"},
+    {"svxname":"CUCC Loser", "svxjs":"caves/expoloser.js"}
 ]); 
