08906eed9017db73470e33fbd8d60f1792a1a59a
Julian Todd <julian@goatchurch.org.uk>
2016-09-05 18:44:44 +0200

    Make 3d option list on opening

---
+++ svx3dlist.js
@@ -0,0 +1,5 @@
+LoadSvx3dList([
+    {"svxname":"rectangular test", "svxjs":"test.js"},
+    {"svxname":"big china cave", "svxjs":"erin2.js"},
+    {"svxname":"maxpleasure2", "svxjs":"maxpleasure2.js"}
+]); 
